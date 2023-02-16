3443f09a8ca5175da8e3c9bd17b22954a195b4d0
KonininaAnastasia <kad28jun2003@mail.ru>
2023-02-16 13:59:25 +0300

    b

--- 2-sem/main.js
+++ 2-sem/main.js
@@ -1,108 +1,139 @@
-console.log("1 задание");
+let zadanie = prompt("Выбери задание:");
+switch(zadanie){
 
-let v_kmch = prompt("Километры в час");
-let v_mc = prompt("Метры в секунду");
+    case '1':
 
-console.log(`${v_kmch} км/ч соответствует ${v_kmch/3.6} м/с`);
-console.log(`${v_mc} м/с соответствует ${v_mc*3.6} км/ч`);
+    console.log("1 задание");
 
-console.log("2 задание");
+    let v_kmch = prompt("Километры в час");
+    let v_mc = prompt("Метры в секунду");
 
-let a = prompt("Сторона 1");
-let b = prompt("Сторона 2");
-let c = prompt("Сторона 3");
+    console.log(`${v_kmch} км/ч соответствует ${v_kmch/3.6} м/с`);
+    console.log(`${v_mc} м/с соответствует ${v_mc*3.6} км/ч`);
 
-if (a+b<c && a+c<b && b+c<a) {
-    console.log("треугольник не существует");
-} else {
-    let p = (a+b+c)/2;
-    let s = Math.sqrt(p*(p-a)*(p-b)*(p-c));
-    console.log("треугольник существует");
-    console.log(`периметр = ${p*2}`);
-    console.log(`площадь = ${s}`);
-    console.log(`соотношение = ${p*2/s}`);
-}
+    break;
 
+    case '2':
 
-console.log("3 задание");
+    console.log("2 задание");
 
-let num = prompt("Введите число");
+    let a = prompt("Сторона 1");
+    let b = prompt("Сторона 2");
+    let c = prompt("Сторона 3");
 
-while (isNaN(num))
-{
-    num = prompt("Введите число");
-}
-
-for (let i=0;i<=num;i++){
-    if (i==0){
-        console.log(`${i} buzz`); 
-    } else if (i%2===0 && i%5!==0){
-        console.log(`${i} buzz`); 
-    } else if(i%2!==0 && i%5!==0){
-        console.log(`${i} fizz`); 
-    } else if(i%2!==0 && i%5===0){
-        console.log(`${i} fizz buzz`);
-    } else if(i%2===0 && i%5!==0){
-        console.log(`${i} fizz`);
-    } else if(i%2===0 && i%5===0){
-        console.log(`${i} fizz buzz`);
+    if (a+b<c && a+c<b && b+c<a) {
+        console.log("треугольник не существует");
+    } else {
+        let p = (a+b+c)/2;
+        let s = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+        console.log("треугольник существует");
+        console.log(`периметр = ${p*2}`);
+        console.log(`площадь = ${s}`);
+        console.log(`соотношение = ${p*2/s}`);
     }
-}
 
-console.log('4 Задание');
+    break;
 
-let str;
-for (let i=1; i<13; i++){
-   if(i%2===0) str="#";
-   else str="*";
-   console.log(`${str.repeat(i)}`);
-}
-console.log('||');
+    case '3':
 
-console.log("5 задание");
+    console.log("3 задание");
 
-let number = prompt("Введите число");
+    let num = prompt("Введите число");
 
-let zagadka = 7;
-
-while (number!=zagadka){
-    if (number>zagadka){
-        console.log(`ваше число больше`);
+    while (isNaN(num))
+    {
+        num = prompt("Введите число");
     }
-    if (number<zagadka){
-        console.log(`ваше число меньше`);
+
+    for (let i=0;i<=num;i++){
+        if (i==0){
+            console.log(`${i} buzz`); 
+        } else if (i%2===0 && i%5!==0){
+            console.log(`${i} buzz`); 
+        } else if(i%2!==0 && i%5!==0){
+            console.log(`${i} fizz`); 
+        } else if(i%2!==0 && i%5===0){
+            console.log(`${i} fizz buzz`);
+        } else if(i%2===0 && i%5!==0){
+            console.log(`${i} fizz`);
+        } else if(i%2===0 && i%5===0){
+            console.log(`${i} fizz buzz`);
+        }
     }
-    number = prompt("Введите число");
+
+    break;
+
+    case '4':
+
+    console.log('4 Задание');
+
+    let str;
+    for (let i=1; i<13; i++){
+    if(i%2===0) str="#";
+    else str="*";
+    console.log(`${str.repeat(i)}`);
+    }
+    console.log('||');
+
+    break;
+
+    case '5':
+
+    console.log("5 задание");
+
+    let number = prompt("Введите число");
+
+    let zagadka = 7;
+
+    while (number!=zagadka){
+        if (number>zagadka){
+            console.log(`ваше число больше`);
+        }
+        if (number<zagadka){
+            console.log(`ваше число меньше`);
+        }
+        number = prompt("Введите число");
+    }
+
+    console.log(`Угадано`);
+
+    break;
+
+    case '6':
+
+    console.log("6 задание");
+
+    let n = prompt("Введите n");
+    let x = prompt("Введите x");
+    let y = prompt("Введите y");
+
+    if (n%x===0 && n%y===0){
+        console.log(`n = ${n}, x = ${x}, y = ${y} => true`);
+    } else {
+        console.log(`n = ${n}, x = ${x}, y = ${y} => false`);
+    }
+
+    break;
+
+    case '7':
+
+    console.log("7 задание");
+
+    let month = prompt("Введите число месяца");
+
+    if (month==1 || month==2 || month==3){
+        console.log(`месяц ${month} => 1 квартал`);
+    }
+    if (month==4 || month==5 || month==6){
+        console.log(`месяц ${month} => 2 квартал`);
+    }
+    if (month==7 || month==8 || month==9){
+        console.log(`месяц ${month} => 3 квартал`);
+    }
+    if (month==10 || month==11 || month==12){
+        console.log(`месяц ${month} => 4 квартал`);
+    }
+
+    break;
+
 }
-
-console.log(`Угадано`);
-
-console.log("6 задание");
-
-let n = prompt("Введите n");
-let x = prompt("Введите x");
-let y = prompt("Введите y");
-
-if (n%x===0 && n%y===0){
-    console.log(`n = ${n}, x = ${x}, y = ${y} => true`);
-} else {
-    console.log(`n = ${n}, x = ${x}, y = ${y} => false`);
-}
-
-console.log("7 задание");
-
-let month = prompt("Введите число месяца");
-
-if (month==1 || month==2 || month==3){
-    console.log(`месяц ${month} => 1 квартал`);
-}
-if (month==4 || month==5 || month==6){
-    console.log(`месяц ${month} => 2 квартал`);
-}
-if (month==7 || month==8 || month==9){
-    console.log(`месяц ${month} => 3 квартал`);
-}
-if (month==10 || month==11 || month==12){
-    console.log(`месяц ${month} => 4 квартал`);
-}
-
