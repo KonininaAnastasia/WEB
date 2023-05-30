69b3481888a9732c0280d4117a3a05109e4bb9fd
KonininaAnastasia <kad28jun2003@mail.ru>
2023-05-30 16:46:42 +0300

    12 laba

---
+++ 2-sem/6/public/Three.js
@@ -0,0 +1,90 @@
+import * as THREE from "https://unpkg.com/three/build/three.module.js";
+
+document.getElementById("table").addEventListener('click',modelview);
+document.getElementById("addModel").addEventListener('click',modelview);
+
+function modelview (event){
+       let canvasWrapper;
+       let typeGeometry;
+       let colorGeometry;
+       let geometry;
+       let sizeGeometry;
+    if(event.target.id==="vie"){
+        typeGeometry = document.getElementById("selectType").value;
+        colorGeometry = document.getElementById("color").value;
+        sizeGeometry = document.getElementById("size").value;
+        if(!sizeGeometry){sizeGeometry=5};
+        let add = document.getElementById("hCan");
+        add.outerHTML ="<div id='hCan'><div id=\"canvas-wrapper\"></div></div>";
+        canvasWrapper = document.getElementById("canvas-wrapper");;
+        //console.log(typeGeometry);
+        //console.log(colorGeometry);
+    }
+    else if(event.target.className==="show") {
+        typeGeometry = document.getElementById("typTable").innerHTML;
+        colorGeometry=document.getElementById("colTable").innerHTML;
+        sizeGeometry=document.getElementById("sizTable").innerHTML;
+        canvasWrapper = document.getElementById("can-wrap");;
+    }
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color("white");
+
+
+    let camera = new THREE.PerspectiveCamera(60, canvasWrapper.clientWidth / canvasWrapper.clientHeight, 1, 1000);
+    let cameraTarget = new THREE.Vector3(0, 0, 0);
+    camera.position.x = 10;
+    camera.position.y = 10;
+    camera.position.z = -10;
+
+    camera.lookAt(cameraTarget);
+    scene.add(camera);
+
+    if(typeGeometry === "cube") {
+        geometry = new THREE.BoxGeometry(sizeGeometry, sizeGeometry, sizeGeometry);
+
+    }else if(typeGeometry==="sphere"){
+        geometry = new THREE.SphereGeometry(sizeGeometry/2);
+    }else{
+        geometry = new THREE.ConeGeometry( Math.sqrt(Math.pow(sizeGeometry,2)/2), Math.sqrt(Math.pow(sizeGeometry,2)/2), 4 );
+    }
+
+    let material = new THREE.MeshPhongMaterial({color: colorGeometry, dithering: true, specular: 0x111111, shininess: 200, side: THREE.DoubleSide});
+    let cube = null;
+    cube = new THREE.Mesh(geometry, material);
+    cube.position.y = 0;
+    cube.position.x = 0;
+    cube.position.z = 0;
+    cube.castShadow = true;
+    scene.add(cube);
+
+    const light = new THREE.PointLight( "#fff", 1, 100);
+    light.position.set(0, 6, -15);
+    light.castShadow = true;
+    scene.add(light);
+
+    const spotLight = new THREE.SpotLight("#ffffff");
+    spotLight.position.set(0, 10, 0);
+    spotLight.castShadow = true;
+    spotLight.intensity = 2;
+    spotLight.shadow.camera.near = 1;
+    spotLight.shadow.camera.far = 25;
+    spotLight.shadow.mapSize.width = 2048;
+    spotLight.shadow.mapSize.height = 2048;
+    spotLight.shadow.bias = -0.01;
+    spotLight.target.position.set(0, 0, 0);
+    scene.add(spotLight);
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(canvasWrapper.clientWidth, canvasWrapper.clientHeight);
+    canvasWrapper.appendChild(renderer.domElement);
+    renderer.shadowMap.enabled = true;
+
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+    animate();
+}
+document.getElementById("table").addEventListener('click',modelview);
+document.getElementById("addModel").addEventListener('click',modelview);
--- 2-sem/6/public/doc.js
+++ 2-sem/6/public/doc.js
@@ -26,13 +26,18 @@ function updTable(){
 update.addEventListener("click", updTable);
 
 let Api;
+let input;
 async function sendName(){
-    let input = document.querySelector('input');
+    input = document.querySelector('input').value;
     let name_user = {
-        name: input.value
+        name: input
     }
-    let response = await fetch('http://127.0.0.1:8000/api3/login', {method: 'POST',headers:{"Content-type":"application/json"}, body: JSON.stringify(name_user)})
-    Api = await response.text();
+    if(input){
+        let response = await fetch('http://127.0.0.1:8000/api3/login', {method: 'POST',headers:{"Content-type":"application/json"}, body: JSON.stringify(name_user)})
+            document.getElementById("addButton").removeAttribute("disabled")
+            //let notify = document.getElementById("notify")
+        Api = await response.text()
+    }
 }
 
 document.getElementById("table").addEventListener('click',function(event) {
@@ -42,7 +47,7 @@ document.getElementById("table").addEventListener('click',function(event) {
 
     if (Api) {fetch(`http://127.0.0.1:8000/api3/models/${id}`, {method: 'DELETE', headers: {"api-key":`${Api}`}}).then(updTable)}
     else {alert("Введите имя..")};
-    console.log(Api);
+    //console.log(Api);
 })
 
 
@@ -60,17 +65,72 @@ document.getElementById("table").addEventListener('click',function(event) {
             if (content[arrayKey]._id === event.target.name){
                 name = event.target.name;
                 flex_div = document.getElementById("flex-div");
-                flex_div.outerHTML = '<table id="flex-div"></table>';
-                flex_div = document.getElementById("flex-div");
-                flex_div.innerHTML += `
-            <tr><td>Имя:</td><td>${content[arrayKey].name}</td></tr>
-            <tr><td>Имя модели:</td><td>${content[arrayKey].name_model}</td></tr>
-            <tr><td>Тип:</td><td>${content[arrayKey].type}</td></tr>
-            <tr><td>Модель:</td><td>${content[arrayKey].model}</td></tr>
-            <tr><td>Описание:</td><td>${content[arrayKey].description}</td></tr> 
-            <tr><td>Комментарии:</td><td>${content[arrayKey].comments}</td></tr>
-            <tr><td>Время создания:</td><td>${content[arrayKey].time_create}</td></tr>`
+                flex_div.outerHTML = `<div id="flex-div"><table>
+                <caption>Модель ${content[arrayKey].name_model}</caption> 
+                <tr><td>Имя:</td><td>${content[arrayKey].name}</td></tr>
+                <tr><td>Имя модели:</td><td>${content[arrayKey].name_model}</td></tr>
+                <tr><td>Тип:</td><td id="typTable" >${content[arrayKey].type}</td></tr>
+                <tr><td>Цвет:</td><td id="colTable">${content[arrayKey].color}</tdid></tr>
+                <tr><td>Размер:</td><td id="sizTable">${content[arrayKey].size}</tdid></tr>
+                <tr><td>Описание:</td><td>${content[arrayKey].descriptions}</td></tr> 
+                <tr><td>Комментарии:</td><td>${content[arrayKey].comments}</td></tr>
+                <tr><td>Время создания:</td><td>${content[arrayKey].time_create}</td></tr></table>
+                <div id="can-wrap"></div></div>`
             };
         }
+    }
+})
+
+document.getElementById("addButton").addEventListener('click',()=>{
+    let add = document.getElementById("addModel")
+         add.innerHTML+=`<div id="formdiv">
+    <form id="form">
+    <p>Добавление модели</p><div>
+    <ul>
+    <li><select id="selectType">
+    <option value="cube">Куб</option>
+    <option value="sphere">Сфера</option>
+    <option value="pyramid">Пирамида</option>
+    </select></li>
+    <li><input id="color" type="color"></li>
+    <li><input type="number" id="size" min="1" max="10" placeholder="Размер модели" required></li>
+    <li><input id="name_model" type="text" placeholder="Имя модели" required></li>
+    <li><input id="descriptions" placeholder="Описание" required></input></li>
+    <li><input id="comments" placeholder="Комментарий" required></input></li>
+    </ul>
+    <span><button type="button" id="submit">Отправить модель</button>
+    <button id="vie" type="button">Посмотеть модель</button></span>
+    </form></div>
+    <div id="hCan"></div>
+    </div>`
+    document.getElementById("addButton").setAttribute('disabled', "")
+    document.getElementById("form").scrollIntoView({block: "center", behavior: "smooth"})
+})
+
+document.getElementById("addModel").addEventListener('click',async (event)=>{
+    if(event.target.id !== "submit") {return 0;}
+    else{
+        let typeGeometry = document.getElementById("selectType").value;
+        let colorGeometry = document.getElementById("color").value;
+        let nameGeometry = document.getElementById("name_model").value;
+        let descriptionsGeometry = document.getElementById("descriptions").value;
+        let commentsGeometry = document.getElementById("comments").value;
+        let sizeGeometry = document.getElementById("size").value;
+        let newModel = {
+            name:input,
+            name_model:nameGeometry,
+            type:typeGeometry,
+            color:colorGeometry,
+            size:sizeGeometry,
+            descriptions:descriptionsGeometry,
+            comments:commentsGeometry
+
+         }
+         await fetch('http://127.0.0.1:8000/api3/models', {method: 'POST', headers: {"api-key": `${Api}`, "Content-type": "application/json"},
+                body: JSON.stringify(newModel)
+            });
+            this.outerHTML = "<div id=\"addModel\"></div>";
+            document.getElementById("submit").type = "submit";
+
     }
 })
